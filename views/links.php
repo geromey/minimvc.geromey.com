ca16180125fd188debd1fe5121af7f86883381cc
geromey <gerome.romey@gmail.com>
2013-04-04 09:08:32 +0200

    add links

--- views/links.php
+++ views/links.php
@@ -1,21 +1,26 @@
 <h1>useful links</h1>
 
-<h2>GitHub</h2>
+<h2>on github</h2>
 
-<h3><a href="https://github.com/geromey/minimvc">github.com/geromey/minimvc</a></h3>
+<li><a href="https://github.com/geromey/minimvc">github.com/geromey/minimvc</a></li>
 
-<p>The source code of minimvc.</p>
+<p>the source code of minimvc.</p>
 
 <p>Note: this does not contains the folder structure. In order to know how to use it, you can watch the repository of this site
 
-
-
-<h3><a href="https://github.com/geromey/minimvc.geromey.com">github.com/geromey/minimvc.geromey.com</a></h3>
+<li><a href="https://github.com/geromey/minimvc.geromey.com">github.com/geromey/minimvc.geromey.com</a></li>
 
 <p>the source code of this site.</p>
 
 <p>This is a good example of how to build a website using minimvc.</p>
 
+<h2>on packagist</h2>
+
+<a href="https://packagist.org/packages/geromey/minimvc">packagist.org/packages/geromey/minimvc</a>
+
+
 <h2>Sites using minimvc</h2>
 
-<a href="http://minimvc.geromey.com">minimvc.geromey.com</a> indeed!
+<ul>
+    <li><a href="http://minimvc.geromey.com">minimvc.geromey.com</a> this web site</li>
+</ul>
